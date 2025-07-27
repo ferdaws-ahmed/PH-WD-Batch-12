6c984db1411e1c5b92b394980235a4f1a696eeca
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2025-07-27 23:31:05 +0600

    satart javascript

---
+++ javaScript/ph-module-13/task-1.js
@@ -0,0 +1,16 @@
+const money = 1000;
+
+const applePrice = 150 * aQuantity;
+const orangePrice = 250 * oQuantity;
+
+const aQuantity = 2;
+const oQuantity =3;
+
+const totalPrice = applePrice + orangePrice;
+
+const remainPrice = money - totalPrice;
+
+// output:
+
+console.log(remainPrice);
+
