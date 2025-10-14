c53e8c779fe661a23dab0a25b1ed817200e70cd8
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2025-10-14 08:34:19 +0600

    project complete

--- milestone-9/ph-12-module-47/simple-firebase-project/src/components/login.jsx
+++ milestone-9/ph-12-module-47/simple-firebase-project/src/components/login.jsx
@@ -1,24 +1,46 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.config';
 const googleProvider = new GoogleAuthProvider;
 const Login = () => {
 
+    const [user, setUser] = useState(null);
     const handleLoginBtn = () =>{
         signInWithPopup(auth, googleProvider)
         .then(result => {
-            console.log(result)
+            
+            setUser(result.user);
         })
         .catch(error =>{
             onclose.log(error)
         });
-        
+        setUser()
+    }
+
+    const handleSingOutBtn = () =>{
+        signOut(auth)
+        .then(()=>{
+           
+            setUser(null)
+        })
     }
     
     return (
         <div>
             <h1>this is login</h1>
-            <button onClick={handleLoginBtn}>SignIn with Gmail</button>
+           
+            
+            {
+                user ? <button onClick={handleSingOutBtn}>Sing Out</button> :  <button onClick={handleLoginBtn}>SignIn with Gmail</button>
+            }
+            
+            {
+                user && <div>
+                    <h1>{user.displayName}</h1>
+                    <p>{user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
