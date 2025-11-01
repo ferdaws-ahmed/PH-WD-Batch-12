15a9dc0b9cfaaf849677f4051313f38b294cc27d
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2025-11-01 22:50:02 +0600

    update module -54

---
+++ milestone-10/ph-12-module-54/simple-crud-server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('simple crud server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`simple crud server is running ${port}`)
+})
